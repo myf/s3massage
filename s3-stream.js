c027c47e921775e8788e767b95677668937d8996
myf <worldwidespirit@gmail.com>
2014-03-19 21:35:12 -0400

    spearating scripts and program

--- s3-stream.js
+++ s3-stream.js
@@ -19,8 +19,29 @@ var load_bucket = function(bucket){
     return s3;
 };
 
+var list_map = function(bucket_name, prefix, start, cb) {
+    var bucket = load_bucket(bucket_name);
+    start = start || '';
+    var lister = new s3Lister(bucket, {
+        prefix: prefix,
+        start: start
+    });
+    var counter = 0;
+    lister
+        .on('error', function (err){
+            console.log(err);
+        })
+        .on('data', function(data) {
+            cb(data.Key, bucket);
+            counter++;
+            console.log(counter, data.Key);
+        })
+        .on('end', function () {
+            console.log('done');
+    });
+};
 
-var copy_data = function(origin_key, bucket){
+var copy_data_formhub = function(origin_key, bucket){
     var re = /^ossap\/attachments\//;
     var photo_file = origin_key.replace(re, '');
     var photo_id = photo_file.replace(/\.jpg$/, '');
@@ -35,6 +56,24 @@ var copy_data = function(origin_key, bucket){
         }
     }).end();
 };
+
+
+var change_size = function(size) {
+    return list_map('nmisfac', size, 'small/1347107879751-small.jpg', function(k, b) {
+        var re = new RegExp(size + '\/');
+        var photo_file = k.replace(re, '');
+        if (!photo_file.match(size)) return;
+        var photo_id = photo_file.replace(/\.jpg$/, '');
+        var id_list = photo_id.split('-');
+        var id = id_list[0];
+        var dest_key = size + '/' + id + '.jpg';
+        b.copy(k, dest_key, function(err, res) {
+            if (err) { console.log(err); }
+        }).end();
+    });
+};
+
+
 
 var size_sel = function(size) {
     switch(size) {
@@ -63,51 +102,6 @@ var copy_data_nmisstatic =  function(original_key, bucket) {
     }
 };
 
-var run_copy = function(start){
-    if (!start) {
-        start = '';
-    }
-    var formhub = load_bucket('formhub');
-    var lister = new s3Lister(formhub, {
-        prefix: 'ossap',
-        start: start
-    });
-    var counter = 0;
-    lister
-        .on('error', function (err){
-            console.log(err);
-        })
-        .on('data', function (data){
-            copy_data(data.Key, formhub);
-            counter++;
-            console.log(counter, data.Key);
-        })
-        .on('end', function () {
-            console.log('done');
-    });
-};
-
-var run_copy_nmisstatic = function(start) {
-    var nmisstatic = load_bucket('nmisstatic');
-    var lister = new s3Lister(nmisstatic, {
-        prefix: 'facimg',
-        start: start
-    });
-    var counter = 0;
-    lister
-        .on('error', function (err){
-            console.log(err);
-        })
-        .on('data', function(data) {
-            copy_data_nmisstatic(data.Key, nmisstatic);
-            counter++;
-            console.log(counter, data.Key);
-        })
-        .on('end', function () {
-            console.log('done');
-    });
-};
-
 var run_delete = function(bucket_name, prefix){
     var bucket = load_bucket(bucket_name);
     var lister = new s3Lister(bucket, {prefix: '' + prefix});
@@ -131,6 +125,4 @@ var run_delete = function(bucket_name, prefix){
 
 };
 
-//run_copy();
-//run_copy_nmisstatic('facimg/8/200/1300983641327.jpg');
-//run_delete('nmisfac', '');
+
